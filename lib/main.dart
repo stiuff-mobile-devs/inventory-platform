b9d44462b1c281e9f3811ee7545ac6b35548d991
reksee-firewall <ramonfernando@id.uff.br>
2024-12-03 23:17:11 -0300

    Update: Configuração inicial do Firebase; Referências em GitIgnore; Alterações em Main pré-implementação de página de Login.

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint("Firebase initialization error: $e");
+  }
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  bool _isDarkTheme = false;
 
-  final String title;
+  final ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue).copyWith(
+      brightness: Brightness.light,
+    ),
+    brightness: Brightness.light,
+    useMaterial3: true,
+  );
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  final ThemeData _darkTheme = ThemeData(
+    primarySwatch: Colors.deepPurple,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+      brightness: Brightness.dark,
+    ),
+    brightness: Brightness.dark,
+    useMaterial3: true,
+  );
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _toggleTheme() {
     setState(() {
-      _counter++;
+      _isDarkTheme = !_isDarkTheme;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Inventory Platform',
+      theme: _isDarkTheme ? _darkTheme : _lightTheme,
+      home: MyHomePage(onChangeTheme: _toggleTheme, isDarkTheme: _isDarkTheme),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final VoidCallback onChangeTheme;
+  final bool isDarkTheme;
+
+  const MyHomePage(
+      {super.key, required this.onChangeTheme, required this.isDarkTheme});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Column(
+            children: [
+              // Header
+              Container(
+                padding: const EdgeInsets.all(16),
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: onChangeTheme,
+                  icon: Icon(
+                    isDarkTheme ? Icons.nightlight_round : Icons.wb_sunny,
+                    color: Colors.white,
+                    size: 36.0,
+                  ),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(
+                      isDarkTheme ? Colors.grey[800] : Colors.blue,
+                    ),
+                    padding: WidgetStateProperty.all(const EdgeInsets.all(10)),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Body
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Welcome to Inventory Platform!',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Content goes here.',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Footer
+              Container(
+                padding: const EdgeInsets.all(16),
+                alignment: Alignment.center,
+                color: isDarkTheme ? Colors.grey[800] : Colors.blue,
+                child: Text(
+                  'Footer content here.',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
