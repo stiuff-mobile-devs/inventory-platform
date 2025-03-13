015425bfdaf05a36803a4eb5c23c8b6e10791688
Igor Deo Alves <igordeo@id.uff.br>
2025-03-12 23:46:57 -0300

    feat: usando HttpService para fazer a chamada e logar

--- lib/core/services/connection_service.dart
+++ lib/core/services/connection_service.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
-import 'package:http/http.dart' as http;
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:inventory_platform/core/debug/logger.dart';
+import 'package:inventory_platform/core/services/http_service.dart';
 
 class ConnectionService {
   final Connectivity _connectivity = Connectivity();
+  final HttpService _httpService = HttpService();
   late StreamSubscription<List<ConnectivityResult>> _connectionSubscription;
 
   final StreamController<bool> _connectionStatusController =
@@ -13,7 +14,7 @@ class ConnectionService {
 
   Future<bool> checkInternetConnection() async {
     try {
-      final response = await http.get(Uri.parse('https://httpbin.org/get'));
+      final response = await _httpService.get('https://httpbin.org/get');
       return response.statusCode == 200;
     } catch (_) {
       return false;
